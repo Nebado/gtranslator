da07f14891c4459cc2b87f1b1e2c5f0cb128899e
Nebado <ruslan.nbd@gmail.com>
2021-08-19 20:00:00 +0100

    Add a fixed position for container and remove container when clicked

--- content/script.js
+++ content/script.js
@@ -1,29 +1,37 @@
 function insertTranslate(text) {
-  let element = document.getElementById('gtranslate');
+    let element = document.getElementById('gtranslate');
 
-  if (element) {
-    element.remove();
-  }
+    if (element) {
+        element.remove();
+    }
 
-  let div = document.createElement('div');
-  div.innerText = text;
-  div.setAttribute('id', 'gtranslate');
-  div.style.position = 'absolute';
-  div.style.fontSize = '12px';
-  div.style.padding = '5px';
-  div.style.background = 'rgba(0,0,0,0.7)';
-  div.style.color = '#fff';
-  div.style.minWidth = '200px';
-  div.style.left = '50%';
-  div.style.top = '50%';
-  div.style.transform = 'translate(-50%, 50%)';
+    let div = document.createElement('div');
+    div.innerText = text;
+    div.setAttribute('id', 'gtranslate');
+    div.style.position = 'fixed';
+    div.style.fontSize = '12px';
+    div.style.padding = '5px';
+    div.style.background = 'rgba(0,0,0,0.8)';
+    div.style.color = '#fff';
+    div.style.minWidth = '200px';
+    div.style.left = '50%';
+    div.style.top = '50%';
+    div.style.transform = 'translate(-50%, 50%)';
 
-  document.body.appendChild(div);
+    document.body.appendChild(div);
+
+    document.addEventListener('click', e => {
+        let element = document.getElementById('gtranslate');
+
+        if (!element.contains(e.target)) {
+            element.remove();
+        }
+    });
 }
 
 browser.runtime.onMessage.addListener((message) => {
-  if (message.command === 'translate') {
-    insertTranslate(message.text);
-  }
+    if (message.command === 'translate') {
+        insertTranslate(message.text);
+    }
 });
 
